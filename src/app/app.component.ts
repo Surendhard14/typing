c7c6ebff4e47524441407c63df16dafc3acdc08f
Surendhar D(surendhar.d) <surendhar.d@infosys.com>
2023-04-27 18:49:26 +0530

    Initial Setup Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { faker } from '@faker-js/faker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = faker.lorem.sentence();
+  enteredText = '';
+  onInput(value: string) {
+    this.enteredText = value;
+  }
+  compare(randomChar: string, enteredChar: string) {
+    if (!enteredChar) {
+      return 'pending';
+    }
+    return randomChar == enteredChar ? 'correct' : 'incorrect';
+  }
 }
